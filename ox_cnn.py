e8a36b1ca385395a1695a13389dd12e8984e4f32
Youngeun Choi <duddms5239@hanyang.ac.kr>
2024-04-15 01:19:08 +0900

    Update ox_cnn.py

--- ox_cnn.py
+++ ox_cnn.py
@@ -8,46 +8,15 @@ import torchvision
 from torchvision import transforms
 import torch.nn as nn
 
-DEVICE = torch.device('cpu')
+DEVICE = torch.device('cpu')  # 기기 설정: CPU 사용
+BATCH_SIZE = 10  # 배치 크기 설정
+EPOCHS = 30  # 에포크 설정
 
-BATCH_SIZE = 10
-EPOCHS = 30
-
-trans = transforms.Compose([
-    transforms.Resize((100, 100)),  
-    transforms.RandomHorizontalFlip(),  # 랜덤하게 이미지를 좌우 반전합니다.
-    transforms.RandomRotation(15),  # 이미지를 최대 15도까지 랜덤하게 회전시킵니다.
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),  # 색상을 랜덤하게 변경합니다.
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-trainset = torchvision.datasets.ImageFolder(root = "/home/zxro/DeepLearning/dataset",
-                                            transform = trans)
-testset = torchvision.datasets.ImageFolder(root = "/home/zxro/DeepLearning/testset", transform = trans)
-
-#trainset.__getitem__(1)
-#print(len(trainset))
-
-classes = trainset.classes
-print(classes)
-
-trainloader = DataLoader(trainset,
-                        batch_size = BATCH_SIZE,
-                        shuffle = True)
-testloader = DataLoader(testset, batch_size = BATCH_SIZE, shuffle = False)
-
-for (X_train, y_train) in trainloader:
-    print('X_train:', X_train.size(), 'type:', X_train.type())
-    print('y_train:', y_train.size(), 'type:', y_train.type())
-    break
-
-pltsize = 1
-plt.figure(figsize=(10 * pltsize, pltsize))
-
+# CNN 모델 정의
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
+        # Convolutional layers
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=5, stride=1, padding=0, bias=True)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
         self.relu1 = nn.ReLU()
@@ -55,79 +24,95 @@ class CNN(nn.Module):
         self.drop2 = nn.Dropout2d(p=0.5)
         self.pool2 = nn.MaxPool2d(kernel_size=2)
         self.relu2 = nn.ReLU()
-
+        # Fully connected layers
         self.fc1 = nn.Linear(in_features=9680, out_features=50, bias=True)
         self.relu1_fc1 = nn.ReLU()
-
         self.fc2 = nn.Linear(in_features=50, out_features=2, bias=True)
         
     def forward(self, x):
         x = self.conv1(x)
         x = self.pool1(x)
         x = self.relu1(x)
-
         x = self.conv2(x)
         x = self.drop2(x)
         x = self.pool2(x)
         x = self.relu2(x)
-
         x = x.view(-1,9680)
-
         x = self.fc1(x)
         x = self.relu1_fc1(x)
-
         x = self.fc2(x)
-
         return x
 
-model = CNN().to(DEVICE)
-
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.00001)
-criterion = nn.CrossEntropyLoss()
-
-print(model)
-
-def train(model, trainloader, optimizer, log_interval):
-    model.train()
+# 모델 학습 함수
+def train(model, trainloader, optimizer, criterion, log_interval, epoch):
+    model.train()  # 모델을 학습 모드로 설정
     for batch_idx, (image, label) in enumerate(trainloader):
-        image = image.to(DEVICE) # 이미지를 GPU로 이동
-        label = label.to(DEVICE) # 레이블을 GPU로 이동
-        optimizer.zero_grad() # 그래디언트 초기화
-        output = model(image) # 모델에 이미지 전달하여 예측, 모델 함수는 Net함수로 train의 main code
-        loss = criterion(output, label) # 손실 계산
-        loss.backward() # 역전파, 이전 텐서와 현재 텐서의 차이, 즉 gradient를 반환
-        optimizer.step() # optimizer에게 loss function를 효율적으로 최소화 할 수 있게 파라미터 수정 위탁
+        image = image.to(DEVICE)  # 이미지를 기기로 이동
+        label = label.to(DEVICE)  # 레이블을 기기로 이동
+        optimizer.zero_grad()  # 그래디언트 초기화
+        output = model(image)  # 모델에 이미지 전달하여 예측
+        loss = criterion(output, label)  # 손실 계산
+        loss.backward()  # 역전파
+        optimizer.step()  # 가중치 업데이트
 
         # 현재 진행 중인 학습 상황 출력
         if batch_idx % log_interval == 0:
-            print("Train Epoch: {} \tTrain Loss: {:.6f}".format(
-                EPOCHS,  
-                loss.item()))
-            
-def evaluate(model, testloader):
-    model.eval()
+            print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                epoch, batch_idx * len(image), len(trainloader.dataset),
+                100. * batch_idx / len(trainloader), loss.item()))
+    
+# 모델 평가 함수
+def evaluate(model, testloader, criterion):
+    model.eval()  # 모델을 평가 모드로 설정
     test_loss = 0
     correct = 0
 
     with torch.no_grad():
         for image, label in testloader:
-            image = image.to(DEVICE) # 이미지를 GPU로 이동
-            label = label.to(DEVICE) # 레이블을 GPU로 이동
-            output = model(image) # 모델에 이미지 전달하여 예측
-            test_loss += criterion(output, label).item() # 손실 누적
-            prediction = output.max(1, keepdim = True)[1] # 가장 높은 확률을 가진 클래스 선택
-            correct += prediction.eq(label.view_as(prediction)).sum().item() # 정확한 예측 수 누적
+            image = image.to(DEVICE)  # 이미지를 기기로 이동
+            label = label.to(DEVICE)  # 레이블을 기기로 이동
+            output = model(image)  # 모델에 이미지 전달하여 예측
+            test_loss += criterion(output, label).item()  # 손실 누적
+            prediction = output.max(1, keepdim = True)[1]  # 가장 높은 확률을 가진 클래스 선택
+            correct += prediction.eq(label.view_as(prediction)).sum().item()  # 정확한 예측 수 누적
     
     # 손실 평균 및 정확도 계산
     test_loss /= (len(testloader.dataset) / BATCH_SIZE)
     test_accuracy = 100. * correct / len(testloader.dataset)
     return test_loss, test_accuracy
 
+# 메인 함수
+def main():
+    # 데이터 변환 정의
+    trans = transforms.Compose([
+        transforms.Resize((100, 100)),  # 이미지 크기 조정
+        transforms.RandomHorizontalFlip(),  # 랜덤하게 이미지 좌우 반전
+        transforms.RandomRotation(15),  # 랜덤하게 이미지 회전
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),  # 색상 조정
+        transforms.ToTensor(),  # 이미지를 텐서로 변환
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # 이미지를 정규화
+    ])
 
-for epoch in range(1, EPOCHS + 1):
-    train(model, trainloader, optimizer, log_interval = 200)
-    test_loss, test_accuracy = evaluate(model, testloader)
-    print("\n[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} % \n".format(
+    # 학습 및 테스트 데이터셋 불러오기
+    # trainset, testset의 root를 자신의 폴더에 맞게 변경해주세요.
+    trainset = torchvision.datasets.ImageFolder(root = "/home/zxro/DeepLearning/dataset",transform = trans)
+    testset = torchvision.datasets.ImageFolder(root = "/home/zxro/DeepLearning/testset", transform = trans)
+    trainloader = DataLoader(trainset, batch_size = BATCH_SIZE, shuffle = True)
+    testloader = DataLoader(testset, batch_size = BATCH_SIZE, shuffle = False)
+
+    model = CNN().to(DEVICE)  # 모델 초기화 및 기기 설정
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.00001)  # 옵티마이저 정의
+    criterion = nn.CrossEntropyLoss()  # 손실 함수 정의
+    print(model)
+
+    # 학습 및 평가 반복
+    for epoch in range(1, EPOCHS + 1):
+        train(model, trainloader, optimizer, criterion, log_interval = 200, epoch=epoch)
+        test_loss, test_accuracy = evaluate(model, testloader, criterion)
+        print("\n[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} % \n".format(
         epoch, test_loss, test_accuracy))
 
-torch.save(model, 'modelcnn.pt')
+    torch.save(model, 'modelcnn.pt')  # 학습된 모델 저장
+
+if __name__ == "__main__":
+    main()
